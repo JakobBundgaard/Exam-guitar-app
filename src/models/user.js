644e922f85a40eecc720ddbe873b16789c7bca6f
JakobBundgaard <jakobbundgaard@hotmail.com>
2021-11-25 06:52:59 +0100

    code cleanup

--- src/models/user.js
+++ src/models/user.js
@@ -78,7 +78,7 @@ userSchema.methods.toJSON = function () {
 userSchema.methods.generateAuthToken = async function () {
     const user = this
     // vi bruger toString fordi det er et objectId, som jwt forventer. hvis ikke får vi binær
-    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN })
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
 
     user.tokens = user.tokens.concat({ token })
     await user.save()
